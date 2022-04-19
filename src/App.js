c0957be2f1f688daeed34ba1415f0a3988bb102e
Prashant singh <71235386+prashant5025@users.noreply.github.com>
2022-04-19 17:15:02 +0530

    next changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React from "react";
 export default class App extends React.Component {
   constructor() {
     super();
-    this.state = { name: "Name" };
+    this.state = { name: "...", email: "xyz@gmail.com", address: "..." };
   }
   componentDidMount() {
     console.warn("componentDidMount");
@@ -15,12 +15,26 @@ export default class App extends React.Component {
 
     return (
       <div className="App">
-        <h1>Component Did Mount</h1>
-        <h1>Name: {this.state.name}</h1>
+        <div className="container">
+          <h1>Component Did Mount</h1>
+          <h2>
+            Name: <span className="answer">{this.state.name} </span>
+          </h2>
+          <h2>
+            Email: <span className="answer">{this.state.email}</span>
+          </h2>
+          <h2>
+            Address: <span className="answer">{this.state.address}</span>
+          </h2>
+        </div>
 
         <button
           onClick={() => {
-            this.setState({ name: "prashant singh" });
+            this.setState({
+              name: "prashant singh",
+              email: "psingh@gmail.com",
+              address: "H.No. 474 Bank coloy hapur 245101"
+            });
           }}
         >
           Update Name
